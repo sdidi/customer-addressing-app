efb16b7487f9ab541a7140dac527f09d255f57d2
Sabre Didi <sabre@red-i.co.za>
2022-03-21 21:42:19 +0200

    added integration tests

--- src/test/java/za/co/addressing/customeraddressing/AddressServiceTests.java
+++ src/test/java/za/co/addressing/customeraddressing/AddressServiceTests.java
@@ -1,24 +1,39 @@
 package za.co.addressing.customeraddressing;
 
-import java.util.Optional;
+import java.util.*;
 import org.junit.*;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.runner.RunWith;
 import org.mockito.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.*;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import za.co.addressing.customeraddressing.model.*;
 import za.co.addressing.customeraddressing.repository.AddressRepository;
 import za.co.addressing.customeraddressing.service.*;
 
 import static net.bytebuddy.matcher.ElementMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith( SpringRunner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class AddressServiceTests
 {
+
+	@Autowired
+	private MockMvc mvc;
+
+	private static Address address;
 	@TestConfiguration
 	static class EmployeeServiceImplTestContextConfiguration {
 
@@ -35,14 +50,23 @@ public class AddressServiceTests
 	private AddressRepository addressRepository;
 
 
-	@Before
+	@BeforeAll
 	public void setUp() {
 		Province province = new Province("WC","ZA","Western Cape");
 		Country country = new Country("ZA","South Africa");
-		Address address = new Address(1L,"3rd ave","2nd stree","Athlone","Cape Town",6650,province,country);
+		address = new Address(1L,"3rd ave","2nd street","Athlone","Cape Town",6650,province,country);
 
 		Mockito.when(addressRepository.findById( 1L ))
 				.thenReturn( Optional.of(address));
+	}
+
+	@Test
+	public void listAllAddresses() throws Exception {
+		List<Address> allAddresses = Arrays.asList(address);
+		Mockito.when(addressService.getAddresses()).thenReturn(allAddresses);
+		mvc.perform( MockMvcRequestBuilders.get("http://localhost:8080/addressapi/addresses").accept( MediaType.APPLICATION_JSON))
+				.andExpect( MockMvcResultMatchers.status().isOk());
+
 	}
 
 	@Test
@@ -50,7 +74,7 @@ public class AddressServiceTests
 		Long id = 1L;
 		Address found = addressService.getAddressById( id );
 
-		//assertThat(false,is(false));
+		assertEquals(false,is(false));
 
 	}
 }
--- src/test/java/za/co/addressing/customeraddressing/controller/AddressControllersIT.java
+++ src/test/java/za/co/addressing/customeraddressing/controller/AddressControllersIT.java
@@ -1,6 +1,5 @@
 package za.co.addressing.customeraddressing.controller;
 
-import java.util.Map;
 import java.util.logging.Logger;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
