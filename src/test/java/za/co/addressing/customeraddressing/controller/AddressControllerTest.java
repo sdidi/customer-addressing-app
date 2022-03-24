6efb3c7ae12816aa8051a2888d6795bac8cfbe69
Sabre Didi <sabre@red-i.co.za>
2022-03-24 07:23:54 +0200

    added further tests

--- src/test/java/za/co/addressing/customeraddressing/controller/AddressControllerTest.java
+++ src/test/java/za/co/addressing/customeraddressing/controller/AddressControllerTest.java
@@ -2,9 +2,11 @@ package za.co.addressing.customeraddressing.controller;
 
 import java.util.*;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,7 +16,7 @@ import za.co.addressing.customeraddressing.service.*;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
-@RunWith( SpringRunner.class )
+@SpringBootTest
 @WebMvcTest(value= AddressController.class )
 public class AddressControllerTest
 {
--- src/test/java/za/co/addressing/customeraddressing/controller/CountryControllerTest.java
+++ src/test/java/za/co/addressing/customeraddressing/controller/CountryControllerTest.java
@@ -2,38 +2,51 @@ package za.co.addressing.customeraddressing.controller;
 
 import java.util.*;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.commons.logging.Logger;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.*;
 import org.springframework.http.*;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit.jupiter.*;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.*;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import za.co.addressing.customeraddressing.controller.CountryController;
 import za.co.addressing.customeraddressing.model.Country;
-import za.co.addressing.customeraddressing.service.CountryService;
+import za.co.addressing.customeraddressing.repository.CountryRepository;
+import za.co.addressing.customeraddressing.service.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
-@RunWith( SpringRunner.class )
-@WebMvcTest(value= CountryController.class )
+@ExtendWith( MockitoExtension.class )
 public class CountryControllerTest
 {
-	@Autowired
-	private MockMvc mockMvc;
-
-	@MockBean
+	@InjectMocks
 	private CountryService countryService;
 
+	@Mock
+	private CountryRepository countryRepository;
+
+/*	@Configuration
+	static class Config {
+		@Bean
+		public CountryService countryService(){
+			return new CountryServiceImpl();
+		}
+	}*/
+
 	Country mockCountry = new Country("ZA","South Africa");
-	String resultCountryJson = "{\"code\":\"ZA\",\"name\":\"South Africa\"}";
 
 	//passed
 	@Test
@@ -49,11 +62,13 @@ public class CountryControllerTest
 		list.add( c3 );
 		list.add( c4 );
 
-		when(countryService.getCountries()).thenReturn( list );
+		when(countryRepository.findAll()).thenReturn( list );
 
 		List<Country> countryList = countryService.getCountries();
 		assertEquals( 4,countryList.size() );
 		verify( countryService, times( 1 ) ).getCountries();
+		var found = countryList.stream().filter(ctr -> "ZA".equals( ctr.getCode()) && "South Africa".equals(ctr.getName())).count() > 0;
+		assertThat(found).isTrue();
 	}
 
 	//passed
